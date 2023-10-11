4c897fe09d0414a9efce7e41474c8199157f15c6
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-10-11 18:27:57 +0200

    add puts ["\n"] in line 40 and 22 to make space and help reading

--- lib/chatbot-chat.rb
+++ lib/chatbot-chat.rb
@@ -12,11 +12,13 @@ headers = {
   "Authorization" => "Bearer #{api_key}"
 }
 
+puts ["\n"]
 puts "Bot de tes Rêves : Quel est votre question ?"
 
 conversation_history = "" # si on utilise un array ça fait planter le bébé
 
 loop do
+  puts ["\n"]
   print "Vous : "
   user_answer = gets.chomp
   conversation_history += user_answer
@@ -34,7 +36,8 @@ loop do
     response = HTTP.post(url, headers: headers, body: data.to_json)
     response_body = JSON.parse(response.body.to_s)
     response_string = response_body['choices'][0]['text'].strip
-  
+    
+    puts ["\n"]
     puts "BDTR : #{response_string}"
 
     conversation_history += response_string
